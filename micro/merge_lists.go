cb6a54e9e0d0bd07c2daa6d0b151bb8c90257861
aptu <asyatsai@gmail.com>
2022-11-06 15:28:15 -0800

    Get server to start and enable reflection for grpcurl testing.

--- micro/merge_lists.go
+++ micro/merge_lists.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-
+	"log"
 	"micro/pb"
+	"net"
+
+	grpc "google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 type server struct {
 	pb.MergeListsServer
 }
 
+func newServer() *server {
+	s := &server{}
+
+	return s
+}
+
 func (s *server) MergeLists(request pb.MergeListRequest) pb.MergeListResponse {
+	fmt.Println("Merging lists...")
 	return pb.MergeListResponse{}
 }
 
@@ -42,10 +54,22 @@ func merge(l1 []int, l2 []int) []int {
 }
 
 func main() {
-	l1 := []int{1, 3, 5, 6}
-	l2 := []int{2, 4, 6, 8}
-	fmt.Println(l1)
-	fmt.Println(l2)
-	l3 := merge(l1, l2)
-	fmt.Println(l3)
+	// l1 := []int{1, 3, 5, 6}
+	// l2 := []int{2, 4, 6, 8}
+	// fmt.Println(l1)
+	// fmt.Println(l2)
+	// l3 := merge(l1, l2)
+	// fmt.Println(l3)
+	fmt.Println(pb.MergeListRequest{})
+
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 7777))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterMergeListsServer(grpcServer, newServer())
+	reflection.Register(grpcServer)
+	grpcServer.Serve(lis)
 }
