a56b3f5177aba5c24550b969480671114e89768b
aptu <asyatsai@gmail.com>
2022-11-06 20:14:53 -0800

    Cleanup server test.

--- micro/merge_lists_test.go
+++ micro/merge_lists_test.go
@@ -1,33 +1,31 @@
 package main
 
 import (
-    "testing"
 	"context"
 	"log"
 	"micro/pb"
+	"testing"
 
 	grpc "google.golang.org/grpc"
 	// "google.golang.org/grpc/credentials/insecure"
 )
 
-
 func TestServer(t *testing.T) {
 
-    // conn, err := grpc.Dial("localhost:7777", grpc.WithTransportCredentials(insecure.NewCredentials()))
-    conn, err := grpc.Dial("localhost:7777", grpc.WithInsecure())
-    defer conn.Close()
+	conn, err := grpc.Dial("0.0.0.0:7777", grpc.WithInsecure())
+	defer conn.Close()
 
-    if err != nil {
-        log.Println(err)
-    }
+	if err != nil {
+		log.Println(err)
+	}
 
-    client := pb.NewMergeListsClient(conn)
-    res, err := client.Merge(context.Background(), &pb.MergeListRequest{
-        List1: &pb.List{V:[]int32 {1, 3, 5, 7}},
-        List2: &pb.List{V:[]int32 {2, 4, 6, 8}},
-        },
-    )
+	client := pb.NewMergeListsClient(conn)
+	res, err := client.Merge(context.Background(), &pb.MergeListRequest{
+		List1: &pb.List{V: []int32{1, 3, 5, 7}},
+		List2: &pb.List{V: []int32{2, 4, 6, 8}},
+	},
+	)
 
-    log.Println(res)
-    log.Println(err)
+	log.Println(res)
+	log.Println(err)
 }
