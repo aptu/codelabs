4c03ef0695ac0a528b0a36078477b243f1f3c1fa
aptu <asyatsai@gmail.com>
2022-11-08 21:47:26 -0800

    Use -n flag to support script execution in Alpine.

--- micro/logger/build.sh
+++ micro/logger/build.sh
@@ -1,3 +1,3 @@
 GEN_DIR=pb
-INC_DIR=$(find ~/go/ -name empty.proto | xargs -l dirname)
+INC_DIR=$(find ~/go/ -name empty.proto | xargs -n1 dirname)
 protoc -I=${INC_DIR} --go_opt=paths=source_relative --go_out=. --go-grpc_opt=paths=source_relative --go-grpc_out=. ./${GEN_DIR}/logger.proto 
