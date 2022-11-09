96aaf315f816cc8f5fa0f678f67f4359f8e41b62
aptu <asyatsai@gmail.com>
2022-11-08 21:42:10 -0800

    Search for proto files when generating protobuf definitions.
    Logger service relies on Empty type and need proto files.

--- micro/logger/build.sh
+++ micro/logger/build.sh
@@ -1,2 +1,3 @@
 GEN_DIR=pb
-protoc --go_opt=paths=source_relative --go_out=. --go-grpc_opt=paths=source_relative --go-grpc_out=. ./${GEN_DIR}/logger.proto 
+INC_DIR=$(find ~/go/ -name empty.proto | xargs -l dirname)
+protoc -I=${INC_DIR} --go_opt=paths=source_relative --go_out=. --go-grpc_opt=paths=source_relative --go-grpc_out=. ./${GEN_DIR}/logger.proto 
